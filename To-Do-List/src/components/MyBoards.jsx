227befb6d40b126e560e3fb598b41231cb950a2a
EzequielMelo <ezequielanthonymelo@gmail.com>
2024-03-20 19:43:17 -0300

    Se implemento una solucion temporal al borrar una boar en myBoards, lo que hace es recargar la pagina para que se vuelva a cargar la lista (intentar buscar otra solucion). Ahora al superar la cantidad de items que se pueden mostrar en las list-containers aparece una scrollbar para deslizarte por los distintos items

--- To-Do-List/src/components/MyBoards.jsx
+++ To-Do-List/src/components/MyBoards.jsx
@@ -2,27 +2,28 @@ import BoardSelection from "./BoardSelection"
 import PropTypes from 'prop-types';
 import { useDragAndDrop } from "@formkit/drag-and-drop/react"; 
 import { animations } from "@formkit/drag-and-drop";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const MyBoards = ({ boards, onBoardSelect, onBoardDelete, onBoardsChange }) => {
   const [parent, list] = useDragAndDrop(boards,{ 
     plugins: [animations()] 
   })
-
-  const listRef = useRef(list);
-
-  useEffect(() => {
-    if (listRef.current !== list) {
-      onBoardsChange(list);
-      listRef.current = list;
-    }
-  }, [list]);
-  /*
+  const navigate = useNavigate();
+  
+  //El problema principal aca es que el .map que se utiliza no es el mismo que es que se muestra
+  //osea me llega boards y se usa list para mostrar solucionar esto es lo que va a resolver el problema
   useEffect(() => {
     onBoardsChange(list)
     console.log(list)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [list]);
-*/
+
+  const handleBoardDelete = (boardId) => {
+    onBoardDelete(boardId)
+    navigate(0);
+  }
+  
   return (
     <div className={`list-container`}>
       <h1 className='flex bg-slate-600 bg-opacity-60 rounded-full w-fit px-[10px] py-[2px] mb-3'>Mis Tableros</h1>
@@ -33,7 +34,7 @@ const MyBoards = ({ boards, onBoardSelect, onBoardDelete, onBoardsChange }) => {
         boardName={board.name}     
         boardLists={board.lists}
         onBoardSelect={() => onBoardSelect(board.id)}
-        onBoardDelete={() => onBoardDelete(board.id)}
+        onBoardDelete={() => handleBoardDelete(board.id)}
         />
       ))}
       </div>
