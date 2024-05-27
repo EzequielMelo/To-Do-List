eb4a29bd707936fb11c2314eaaeb22ed66bdc3f0
EzequielMelo <ezequielanthonymelo@gmail.com>
2024-05-26 23:39:09 -0300

    cambios de indentacion en Data.jsx

--- To-Do-List/src/components/Data.jsx
+++ To-Do-List/src/components/Data.jsx
@@ -7,40 +7,40 @@ import { MdOutlineDashboardCustomize } from "react-icons/md";
 
 
 export const datas = [
-    {
+  {
     id: 1,
     icon: <AiOutlineHome />,
     text: "Inicio",
     href: "/inicio"
-    },
-    {
+  },
+  {
     id: 2,
     icon: <MdOutlineDashboardCustomize />,
     text: "Nuevo Tablero",
     href: "/inicio"
-    },
-    {
+  },
+  {
     id: 3,
     icon: <TbListCheck />,
     text: "Nueva Lista",
-    },
-    {
+  },
+  {
     id: 4,
     icon: <FaCheckDouble />,
     text: "Completadas",
     href: "/completadas"
-    },
-    {
+  },
+  {
     id: 5,
     icon: <RxDashboard />,
     text: "Mis Tableros",
     href: "/mis-tableros"
-    },
-    {
+  },
+  {
     id: 6,
     icon: <FiLogOut />,
     text: "Salir",
     href: "/salir"
-    },
-    
+  },
+
 ];
